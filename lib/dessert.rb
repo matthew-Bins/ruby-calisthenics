9ce1233ffa96f6a910a717c14bbeac1c4e436234
Jordan <Murrayjordan96@gmail.com>
2018-02-01 18:20:52 -0500

    This code passes the rspec test cases.

--- lib/dessert.rb
+++ lib/dessert.rb
@@ -1,17 +1,63 @@
 class Dessert
-  def initialize(name, calories)
-    # your code here
-  end
-  def healthy?
-    # your code here
-  end
-  def delicious?
-    # your code here
-  end
+ 	def initialize(name, calories)
+		# YOUR CODE HERE
+		@name = name
+		@calories = calories
+	end
+
+	#setter method
+	def name=(newname)
+		@name = newname
+	end
+	#getter method
+	def name
+		@name
+	end
+
+	def calories
+		@calories
+	end
+
+	#setter method
+	def calories=(newcals)
+		@calories = newcals
+	end
+
+
+ 	def healthy?
+    	# YOUR CODE HERE
+		@calories < 200
+  	end
+
+  	def delicious?
+    	# YOUR CODE HERE
+		return true # returns true for all
+  	end
 end
 
+#part B
 class JellyBean < Dessert
-  def initialize(flavor)
-    # your code here
-  end
+ 	def initialize(flavor)
+    	# YOUR CODE HERE
+		@name = "#{flavor} jelly bean" 
+		@calories = 5 
+		@flavor = flavor
+  	end
+
+	def flavor
+		@flavor
+	end
+
+	def flavor=(newflavor)
+		@flavor = newflavor
+  	end
+
+	def delicious?
+    	# YOUR CODE HERE
+		if @flavor.include?("licorice")
+			return false
+		else
+			return true
+		end
+  	end
 end
--- lib/fun_with_strings.rb
+++ lib/fun_with_strings.rb
@@ -1,17 +1,29 @@
 module FunWithStrings
+
   def palindrome?
     # your code here
+	test = self.downcase.gsub(/[^a-z]*\s*\d*/, "")
+	return test == test.reverse
   end
-  def count_words
-    # your code here
+
+  def count_words # your code here
+	wordhash = Hash.new
+		
+	self.downcase.scan(/\b\w*\b/) do |word|
+		if wordhash.has_key?(word) 
+			wordhash[word] += 1
+		elsif word == ""
+		else wordhash[word] = 1
+		end
+	end
+	return wordhash
   end
+
   def anagram_groups
-    # your code here
+	g = self.downcase.split.group_by{ |x| x.chars.sort }.values
   end
-end
-
 # make all the above functions available as instance methods on Strings:
-
+end
 class String
   include FunWithStrings
 end
